50161335d868de067bd47ad33e84c2f9603a275c
Fernando Gimeno <fernandogimeno.ief@gmail.com>
2024-11-03 15:34:36 -0300

    Endpoints to create, read, update and delete users

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,42 @@
 */
 
 const { Router } = require("express");
-const { getUsers } = require("../controllers/users");
+const { check } = require("express-validator");
+const { validateFields } = require("../middlewares/validate-fields");
+const {
+  createUser,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/users");
 
 const router = Router();
 
-router.get('/', getUsers);
-
+router.post(
+  "/",
+  [
+    check("name", "The name is required").not().isEmpty(),
+    check("email", "The email is required").isEmail(),
+    check("password", "The password is required")
+      .not()
+      .isEmpty()
+      .isLength({ min: 6 }),
+    validateFields,
+  ],
+  createUser
+);
+router.get("/", getUsers);
+router.get("/:uid", getUser);
+router.put(
+  "/:uid",
+  [
+    check("name", "The name is required").not().isEmpty(),
+    check("email", "The email is required").isEmail(),
+    check("role", "The role is required").not().isEmpty(),
+  ],
+  updateUser
+);
+router.delete("/:id", deleteUser);
 
 module.exports = router;
